37c362df434604499a6af02d9938073253d48e45
masudai-code <masunaga0424@gmail.com>
2025-03-02 15:00:54 +0900

    fv-SP完成 PC途中まで

--- src/js/script.js
+++ src/js/script.js
@@ -31,13 +31,13 @@ jQuery(function ($) {
 
     // FV
     const fv__swiper = new Swiper(".js-fv-swiper", {
-      direction: "vertical",
-      loop: true,
-      speed: 2000,
-      autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-      },
+      // direction: "vertical",
+      // loop: true,
+      // speed: 2000,
+      // autoplay: {
+      //   delay: 4000,
+      //   disableOnInteraction: false,
+      // },
       allowTouchMove: false,
     });
   });
