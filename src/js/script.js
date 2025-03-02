77ec063e7538017910fdf503b33a9fe1b2cbde09
masudai-code <masunaga0424@gmail.com>
2025-03-02 10:20:03 +0900

    ヘッダー完了

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,32 @@
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる// ハンバーガーメニュー
+  $(function () {
+    $(".js-hamburger").click(function () {
+      $(this).toggleClass("is-open");
+      $(".js-drawer").toggleClass("is-open");
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+      // ドロワーが開いたときにロゴを変更
+      if ($(".js-drawer").hasClass("is-open")) {
+        $(".header__logo img").attr("src", "./assets/images/common/logo02.svg");
+      } else {
+        $(".header__logo img").attr("src", "./assets/images/common/logo01.svg");
+      }
+    });
 
+    $(".js-drawer a[href]").on("click", function () {
+      $(".js-hamburger").removeClass("is-open");
+      $(".js-drawer").removeClass("is-open");
+      $(".header__logo img").attr("src", "./assets/images/common/logo01.svg");
+    });
 
+    // resizeイベント
+    $(window).on("resize", function () {
+      if (window.matchMedia("(min-width: 768px)").matches) {
+        // PC表示時はメニューを閉じる
+        $(".js-hamburger").removeClass("is-open");
+        $(".js-drawer").removeClass("is-open");
+        $(".header__logo img").attr("src", "./assets/images/common/logo01.svg");
+      }
+    });
+  });
 });
